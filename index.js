e3d28b52188c4a7613285da2e6e9b695489c1e64
Razutzu <81825697+Razutzu@users.noreply.github.com>
2025-05-11 17:40:55 +0300

    commit

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const d = document.getElementById("days").children.item(0);
-const h = document.getElementById("hrs").children.item(0);
-const m = document.getElementById("mins").children.item(0);
-const s = document.getElementById("secs").children.item(0);
+const d = document.getElementById("days");
+const h = document.getElementById("hrs");
+const m = document.getElementById("mins");
+const s = document.getElementById("secs");
+const ms = document.getElementById("ms");
 
-const continer = document.getElementById("container");
-const progressContainer = document.getElementById("progress");
-
-const relDate = new Date("November 13, 2023, 21:35:00 UTC+2").getTime();
-const start = new Date("January 13, 2024, 21:35:00 UTC+3").getTime();
-const reach = new Date("February 13, 2025, 21:35:00 UTC+3").getTime();
+const relDate = new Date("May 13, 2025, 21:35:00 UTC+2").getTime();
 
 function displayTime(showTime) {
-	let time = showTime - relDate;
+	let time = relDate - showTime;
 
 	const days = Math.floor(time / 86400000);
 	time -= days * 86400000;
@@ -20,12 +16,15 @@ function displayTime(showTime) {
 	const minutes = Math.floor(time / 60000);
 	time -= minutes * 60000;
 	const seconds = Math.floor(time / 1000);
+	time -= seconds * 1000;
+	const mseconds = Math.floor(time / 10);
 
 	d.innerHTML = days > 9 ? days : `0${days}`;
 	h.innerHTML = hours > 9 ? hours : `0${hours}`;
 	m.innerHTML = minutes > 9 ? minutes : `0${minutes}`;
 	s.innerHTML = seconds > 9 ? seconds : `0${seconds}`;
+	ms.innerHTML = mseconds > 9 ? mseconds : `0${mseconds}`;
 }
 
 displayTime(Date.now());
-setInterval(() => displayTime(Date.now()), 100);
+setInterval(() => displayTime(Date.now()), 10);
